cc5d573fbfd6b4afffc8cc734f934b824ae1fbdc
Levi Gist <ligist2@gmail.com>
2024-03-06 16:48:26 -0600

    bmi_calculate tests

--- bmi.py
+++ bmi.py
@@ -1,33 +1,45 @@
 
 
 class BMI():
-    def __init__(self, weight: int, height_feet: int, height_inches: int):
+    def __init__(self, weight: int, height_feet: int, remaining_height_inches: int):
         self.weight = weight
         self.height_feet = height_feet
-        self.height_inches = height_inches
+        self.remaining_height_inches = remaining_height_inches
 
     
     def bmi_calculate(self) -> float:
-        feet_inch = self.height_feet * 12
-        weight_kg = self.weight * 0.45 #weight from lbs to kg
-        height_m = ((self.height_inches + feet_inch) * 0.025)**2 #height from ft/in to meters
-        final_bmi = round(weight_kg / height_m, 1) #get bmi
+        try:
+            feet_inch = self.height_feet * 12
+            weight_kg = self.weight * 0.45 #weight from lbs to kg
+            height_m = ((self.remaining_height_inches + feet_inch) * 0.025)**2 #height from ft/in to meters
+            final_bmi = round(weight_kg / height_m, 1) #get bmi
 
-        return final_bmi
+            return final_bmi
+        except TypeError:
+            return "ERROR: Please use numbers only."
+
+        
 
 
-    def bmi_category(self) -> str:
+    def bmi_category(self, bmi: float = None) -> str:
+        #can pass in an optional specific bmi
+        #argument will override the default calculated bmi
         category = ['Underweight','Normal Weight', 'Overweight', 'Obese' ]
-        bmi = self.bmi_calculate()
-        if bmi < 18.5:
-            return category[0]
-        elif bmi >= 18.5 and bmi <= 24.9:
-            return category[1]
-        elif bmi >= 25 and bmi <= 29.9:
-            return category[2]
-        elif bmi >= 30:
-            return category[3]
-
+        if bmi != None:
+            bmi = bmi
+        else:
+            bmi = self.bmi_calculate()
+        try:
+            if bmi < 18.5:
+                return category[0]
+            elif bmi >= 18.5 and bmi <= 24.9:
+                return category[1]
+            elif bmi >= 25 and bmi <= 29.9:
+                return category[2]
+            elif bmi >= 30:
+                return category[3]
+        except TypeError:
+            return "ERROR: Unable to receive bmi category. Please use numbers only."
 
     def display_bmi_info(self) -> str:
         bmi = self.bmi_calculate()
@@ -37,10 +49,11 @@ class BMI():
 
 
 def main():
-   bmi = BMI(125, 5, 3)
-
-   print(bmi.get_bmi_category())
-   print(bmi.display_bmi_info())
+    ...
+#    bmi = BMI(125, 5, 3)
+#    print(bmi.bmi_calculate())
+#    print(bmi.bmi_category())
+#    print(bmi.display_bmi_info())
 
 
 
--- test_bmi.py
+++ test_bmi.py
@@ -1,2 +1,18 @@
 from bmi import BMI
+import pytest
+
+
+#Tests form bmi_calculate()
+@pytest.mark.parametrize(
+        "weight, height, height_inch, output",
+        [
+            (125, 5, 3, 22.7),#TC Calculate 01.1 -> test accuracy
+            (125, "a", 3, "ERROR: Please use numbers only.")#TC Calculate 01.2 -> tests caught exception
+        ]
+)
+def test_bmi_calculate(weight, height, height_inch, output):
+    bmi = BMI(weight, height, height_inch)
+    bmi_calc = bmi.bmi_calculate()
+    assert bmi_calc == output
+
 
